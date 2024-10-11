206289cf736fb27c56525b1e30607933ca386c01
ErikaOlsson <erika__olsson@hotmail.com>
2024-10-11 14:17:22 +0200

    Update: grid is working, img and album name

--- src/components/MusicGallery.jsx
+++ src/components/MusicGallery.jsx
@@ -1,28 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import data from '../data.json';
 import './MusicGallery.css';
-
+import dotsIcon from '../assets/icons/dots.svg'
+import heartIcon from '../assets/icons/heart.svg'
+import playIcon from '../assets/icons/play.svg'
 
 
 console.log(data);
 
 const MusicGallery = () => {
     
-  /*   const [albums, setAlbums] = useState([]);
+    /* const [albums, setAlbums] = useState([]);
 
     // Load the data directly from the imported JSON file
     useEffect(() => {
         setAlbums(data); // Directly use the imported data
-    }, []);
- */
+    }, []); */
+
     return (
         <>
         <div className="album-grid-container">
           {data.albums.items.map((album) => (
             <div key={album} className="album-card">
-                <img src={data.albums.images?.[0]?.url} alt={album.name} className="album-image" />
-                <h3 className="album-name">{data.albums.name}</h3>
-                <p className="album-artist">{data.albums.artist}</p>
+                <div className="album-cover-hover">
+                    <img src={album.images?.[0]?.url} alt={album.name} className="album-image" />
+                    <div className="album-cover-icons">
+                        <img src={heartIcon} width="30" alt="Icon - Like album" />
+                        <img src={playIcon} width="60" alt="Icon - Play album" />
+                        <img src={dotsIcon} width="30" alt="Icon - Other" />
+                    </div>
+                </div>
+                <h3 className="album-name">{album.name}</h3>
+                <p className="album-artist">{album.artists?.[0]}</p>
             </div>
           ))}
         </div>
